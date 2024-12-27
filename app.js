83f59c9835e705a2f12fb7175613a34abe9cbce4
Alan Chelko <achelko@gmail.com>
2024-12-27 15:49:42 +0000

    Initial MDB connection setup

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
-
-
+const Campground = require('./models/campground.model')
+const connectDB = require('./config/db.js');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,6 +14,18 @@ app.get('/', (req, res) => {
   res.render('home');
 })
 
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({
+    title: 'Prince Gallitzin',
+    description: "Squirrel feeding!"
+  })
+  await camp.save();
+  res.send(camp)
+  console.log(camp);
+})
+
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is open on port ${PORT}`)
 })
---
+++ models/campground.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const campgroundSchema = new mongoose.Schema({
+  title: String,
+  price: String,
+  description: String,
+  location: String
+})
+
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+// standard is to name 'Product' (capitalized and singular), and then mongoose makes it to plural lower case
+
+module.exports = Campground;
+
+// const campgroundSchema = new mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true
+//   },
+//   price: {
+//     type: Number,
+//     required: true
+//   },
+//   description: {
+//     type: String,
+//     required: true
+//   },
+//   location: {
+//     type: String,
+//     required: true
+//   }
+// }, {
+//   timestamps: true // createdAt, updatedAt fields on each document
+// })
+
