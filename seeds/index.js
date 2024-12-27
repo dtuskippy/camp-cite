8aa8426a19eb7fc4b7b0536d02b8dc2ab8c95a05
Alan Chelko <achelko@gmail.com>
2024-12-27 21:17:30 +0000

    Database seeding folder and files

---
+++ seeds/index.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground.model');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // process code 1 means exit with failure, 0 means success
+  }
+}
+
+// mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true
+// });
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//   console.log("Database connected");
+// });
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
+
+
+connectDB();
